ee3adb9aa94f2faaac03facc8aade4bc9d0d6cbb
chenwp <chenwp@Ctrip.com>
2016-03-28 17:44:35 +0800

    header区域都由外部传入, 本身不做处理.

--- index.js
+++ index.js
@@ -5,24 +5,17 @@
  */
 var through2 = require('through2');
 
-module.exports = function (options) {
-
-
-
-  return through2.obj(function (file, enc, callback) {
-    if (file.isNull()) {
-      return callback(null, file);
-    }
-    var str = file.contents.toString();
-    var _ainfo = [];
-    _ainfo.push('/* ');
-    _ainfo.push(new Date().toString());
-    _ainfo.push(' '+ options);
-    _ainfo.push(' */');
-    _ainfo.push("\r\n");
-    _ainfo.push(str);
-    var newstr = _ainfo.join('');
-    file.contents = new Buffer(newstr);
-  	callback(null, file);
-  });
+module.exports = function(options) {
+    return through2.obj(function(file, enc, callback) {
+        if (file.isNull()) {
+            return callback(null, file);
+        }
+        var str = file.contents.toString();
+        var _ainfo = [];
+        _ainfo.push(options);
+        _ainfo.push(str);
+        var newstr = _ainfo.join('');
+        file.contents = new Buffer(newstr);
+        callback(null, file);
+    });
 };

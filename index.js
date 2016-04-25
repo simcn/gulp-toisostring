a4ed0ce2d6812a4cee9246fcec503f83629281b2
chenwp <chenwp@Ctrip.com>
2016-04-25 13:30:33 +0800

    增加 __md5__ 与 __time__ 用当前时间替换的功能.

--- index.js
+++ index.js
@@ -5,6 +5,31 @@
  */
 var through2 = require('through2');
 
+Date.prototype.cdn = function() {
+    var D = [
+        this.getFullYear(),
+        (this.getMonth() + 1),
+        this.getDate(),
+        this.getHours(),
+        this.getMinutes(),
+        this.getSeconds()
+    ];
+    return D.join('');
+};
+
+var CDN = new Date().cdn();
+
+/**
+ * 替换内容标记的时间戳
+ * @param  {[type]} str [description]
+ * @return {[type]}     [description]
+ */
+var replaceCDN = function(str){
+    str = str.replace(/__md5__/g, CDN);
+    str = str.replace(/__time__/g, CDN);
+    return str;
+};
+
 module.exports = function(options) {
     return through2.obj(function(file, enc, callback) {
         if (file.isNull()) {
